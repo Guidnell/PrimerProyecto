91de1bb4735cbcd0b8891e314f6f29c1ab6437c5
Guidnell <guidnell21@gmail.com>
2026-01-27 19:18:16 -0600

    Carga inicial

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PrincipalController;
+
+
+Route::get('/hello',HomeController::class);
+Route::get('post/mensaje',[PostController::class,
+'Mensaje']);/*Si el usuario escribe 'post/hello' ingresara al método 'Mensaje'*/
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
